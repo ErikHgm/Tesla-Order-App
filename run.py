8062515a2994641ec1356136f5484e6cec382806
Erik <erikhgm@gmail.com>
2022-06-27 09:42:37 +0000

    Refactor all functions using lists instead of flow control

--- run.py
+++ run.py
@@ -7,22 +7,11 @@ def show_car_models():
     """
     print('Which car model would you like to order? Choose between option 1-4. \n\n1. Model S ($89000)\n2. Model X ($95000)\n3. Model 3 ($49000)\n4. Model Y ($55000)\n')
     user_choice = input('Enter your option here: ')
-    print(f'\nYou picked car model: {user_choice}') 
-    price = 0
-    model = ''
-    if user_choice == '1':
-        price = 89000
-        model = 'Model S' 
-    elif user_choice == '2':
-        price = 95000
-        model = 'Model X' 
-    elif user_choice == '3':
-        price = 49000
-        model = 'Model 3' 
-    else:
-        price = 55000
-        model = 'Model Y'      
-    return price, model
+    model_options = ['Model S', 'Model X', 'Model 3', 'Model Y']
+    model = model_options[int(user_choice)-1]
+    print(f'\nYou have selected model: {model}\n')  
+
+    return model
 
 
 def show_color_options():
@@ -30,24 +19,13 @@ def show_color_options():
     Shows user the different options for colors. 
     Returns the price and color that the user chose.    
     """
-    print()
     print('Which color would you like? Choose between option 1-3. \n\n1. Black (standard)\n2. Titanium Grey (+ $500)\n3. Pearl White (+ $1000)\n')
     user_choice = input('Enter your option here: ')
-    price = 0
-    color = ''
-    if user_choice == '1':
-        price = 0
-        color = 'Black' 
-    elif user_choice == '2':
-        price = 500
-        color = 'Titanium Grey' 
-    else:
-        price = 1000
-        color = 'Pearl White'   
-    
-    print(f'\nYou picked: {color}')       
-   
-    return price, color
+    color_options = ['Black', 'Titanium Grey', 'Pearl White']
+    color = color_options[int(user_choice)-1]
+    print(f'\nYou have selected color: {color}\n')  
+
+    return color
 
 
 def show_drivetrain_options():
@@ -55,19 +33,13 @@ def show_drivetrain_options():
     Shows user the different drivetrain options. 
     Returns the price and drivetrain that the user chose.    
     """
-    print()
     print('Which drivetrain option would you like? Choose between option 1-2. \n\n1. 2WD (standard)\n2. 4WD (+ $2000)\n')
     user_choice = input('Enter your option here: ')
-    print(f'\nYou picked drivetrain: {user_choice}')       
-    price = 0
-    drivetrain = ''
-    if user_choice == '1':
-        price = 0
-        drivetrain = '2WD' 
-    else:
-        price = 2000
-        drivetrain = '4WD'      
-    return price, drivetrain
+    drivetrain_options = ['2WD', '4WD']
+    drivetrain = drivetrain_options[int(user_choice)-1]
+    print(f'\nYou have selected drivetrain: {drivetrain}\n')  
+
+    return drivetrain
 
 
 def show_interior_options():
@@ -75,20 +47,13 @@ def show_interior_options():
     Shows user the different interior options. 
     Returns the price and interior that the user chose.
     """
-    print()
     print('Which interior color would you like? Choose between option 1-2. \n\n1. Black (standard)\n2. White (+ $2000) \n')
     user_choice = input('Enter your option here: ')
-    print(f'\nYou picked interior: {user_choice}')       
-    price = 0
-    interior = ''
-    if user_choice == '1':
-        #price = 0
-        interior = 'Black' 
-        return interior
-    else:
-        price = 2000
-        interior = 'White'      
-        return price, interior
+    interior_options = ['Black', 'White']
+    interior = interior_options[int(user_choice)-1]
+    print(f'\nYou have selected interior: {interior}\n')  
+
+    return interior
 
 
 def print_receipt(car, color, drivetrain, interior):
@@ -119,8 +84,6 @@ def main():
     drivetrain_option = show_drivetrain_options()
     interior_option = show_interior_options()
 
-    print_receipt(car_option, color_option, drivetrain_option, interior_option)
- 
 
 main()
 
